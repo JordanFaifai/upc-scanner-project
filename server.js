a656096076150f982feb456685e02573609857bc
Jordan Faifai <jordanfaifai@gmail.com>
2025-05-20 18:46:12 -0700

    Logging actual listening port

--- server.js
+++ server.js
@@ -104,4 +104,5 @@ app.get('/api/ingredients/:upc', async (req, res) => {
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+ console.log(`[SERVER] Actual listening PORT: ${PORT}`); // <--- ADD THIS LINE
 });
