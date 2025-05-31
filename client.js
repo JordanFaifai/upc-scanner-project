80261feb3e8f6c0ee559474575b0601be3cfcdac
Jordan Faifai <jordanfaifai@gmail.com>
2025-05-31 10:52:49 -0700

    FIX

--- client.js
+++ client.js
@@ -1362,6 +1362,6 @@ function getNovaGroupReminderHtml() {
                 <p class="note">Pure Scan helps you identify NOVA groups so you can make more informed dietary choices!</p>
             </div>
         </div>
-    `; //
-} //
-
+    `; 
+} 
+}
